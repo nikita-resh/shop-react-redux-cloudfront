d55591f5fcc7c7f4af77d483ace493a61d32f79f
nikita-resh <nikita.reshetnikov9@gmail.com>
2022-07-02 21:37:51 +0400

    feat: add import file functionality

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
 	base: 'https://2y76f36ehe.execute-api.eu-west-1.amazonaws.com/dev',
 	product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	import: 'https://dha5n62l87.execute-api.eu-west-1.amazonaws.com/dev/import',
 	bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
